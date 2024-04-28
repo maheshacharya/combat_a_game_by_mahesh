fa3b524f43bacb2ac28693a0a4282122038b1288
Mahesh Acharya <maheshacharya@hotmail.com>
2024-04-27 20:06:19 -0400

    Update combain_main.pas

--- combain_main.pas
+++ combain_main.pas
@@ -3,31 +3,31 @@ label 10 :
 Var
   X, init hide, init xtank, init tank ,init_sound,
   Xtank value, Ytank_value, value sound, value_delay, loop ,where-hide, satellite, where Xtank ,where_Ytank, random note,term,plane-no, center_X ,center_Y:integer;
-  missile-allow, game-over, sat move, temp_term, done :bool ean;
-  board_no, tank_no,sat_random, score:integer:
-  ch : string[1]
+  missile-allow, game-over, sat move, temp_term, done :boolean;
+  board_no, tank_no,sat_random, score:integer;
+  ch : string[1];
   temp-hide,
-  hide : array [0..31] of boolean
+  hide : array [0..31] of boolean;
   Xtank,
-  Ytankarray [0..31] of integer.;
+  Ytankarray [0..31] of integer;
   array-sound,array-delay :array [0..6] of integer;
   bomb: array [0..7] of boolean;
   Xbomb,
-  Ybomb: array [0..7] of integer:
+  Ybomb: array [0..7] of integer;
   terminated array [0..4] of boolean;
   Xconst_ planel,
   Xeonst_plane2,
   Xconst plane3,
   Xconst,_plane4,
   Yconst_Dlane: integer,
-  missile, Ymissile : integer
+  missile, Ymissile : integer;
   s,c，
   plane,
   satel11te：
-  backtank ,black_sat, key.
-  protank: array L 0 .. 1000 ] of byte:
-  tankshells, hidetank ,blackplane:arrayLO..1000] of byte :
-  bak,pro : boolean i
-  Xplane, Yplane integer ;
+  backtank ,black_sat, key,
+  protank: array [0 .. 1000] of byte;
+  tankshells, hidetank ,blackplane:array[0..1000] of byte;
+  bak,pro : boolean i;
+  Xplane, Yplane integer;
 (*
 procedures )
